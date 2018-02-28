4edf131971e9ddebf6d89dd903b7fbdfb5966b96
罗奕斐 <gdutyifei@163.com>
2018-02-28 20:21:36 +0800

    token增加type字段

--- db/access_token.sql
+++ db/access_token.sql
@@ -6,6 +6,7 @@ CREATE TABLE `access_token` (
     `updated_by` VARCHAR(30) NULL DEFAULT NULL COLLATE 'utf8mb4_bin',
     `updated_date` DATETIME NULL DEFAULT NULL,
     `access_token` VARCHAR(520) NULL DEFAULT NULL COMMENT 'access_token' COLLATE 'utf8mb4_unicode_ci',
+    `token_type` VARCHAR(5) NULL DEFAULT NULL COMMENT 'access_token' COLLATE 'utf8mb4_unicode_ci',
     PRIMARY KEY (`id`)
 )
 COLLATE='utf8mb4_bin'
